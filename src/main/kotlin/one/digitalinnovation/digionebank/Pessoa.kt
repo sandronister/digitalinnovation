d3358eba39ee026fbd326e3d71cd333d5e94fc39
Júlio Sandron <sandronister@icloud.com>
2021-02-21 17:59:57 -0300

    private set

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,23 +2,18 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome:String=""
-    var cpf:String=""
 
-    inner class Endereco{
-        var rua:String="Av Tambaqui"
-        var numero:Number=999
-    }
+    var cpf:String="999.999.99-99"
+    private set
+
 }
 
 
 fun main() {
     val p = Pessoa()
     p.nome = "Júlio Sandroni"
-    p.cpf = "123.123.123-23"
 
 
     println(p.nome)
     println(p.cpf)
-    println(p.Endereco().rua)
-    println(p.Endereco().numero)
 }
